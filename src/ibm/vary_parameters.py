08e19ca28a1953db136acf9d0df36d65f673549e
bramkuijper <bramkuijper@gmail.com>
2026-08-28 22:07:05 +0100

    small changes

--- src/ibm/vary_parameters.py
+++ src/ibm/vary_parameters.py
@@ -6,25 +6,20 @@ base_name = "sim_group_forage_"
 current_time = dt.datetime.now()
 base_name += current_time.strftime("%Y%m%d_%H%M%S")
 
-nrep = 5
+nrep = 10
 
 sim_counter = 0
 
 # resources at the start of the season
 init_resources = 10
 nest_pred_baseline = 0.0
-nest_pred_scale = list(np.linspace(0,0.1,10))
+nest_pred_scale = [0.05]
 
 mu_a_resource = [0.05]
-mu_b_resource = [0.05]
-mu_a_t = [0.05]
-mu_b_t = [0.05]
-mu_a_qown = [0]
-mu_b_qown = [0]
-mu_a_qother = [0]
-mu_b_qother = [0]
-mu_a_action_other = [0]
-mu_b_action_other = [0]
+mu_a_t = [0,0.05]
+mu_a_qown = [0,0.05]
+mu_a_qother = [0,0.05]
+mu_a_action_other = [0,0.05]
 
 p_high_quality = [0.5]
 max_gen = 50000
@@ -33,37 +28,42 @@ exe = "./group_foraging_ibm.exe"
 
 for i in range(0, nrep):
     for mu_a_resource_i in mu_a_resource:
-        for mu_b_resource_i in mu_b_resource:
-            for mu_a_t_i in mu_a_t:
-                for mu_b_t_i in mu_b_t:
-                    for mu_a_qown_i in mu_a_qown:
-                        for mu_b_qown_i in mu_b_qown:
-                            for mu_a_qother_i in mu_a_qother:
-                                for mu_b_qother_i in mu_b_qother:
-                                    for mu_a_action_other_i in mu_a_action_other:
-                                        for mu_b_action_other_i in mu_b_action_other:
-                                            for p_high_quality_i in p_high_quality:
-                                                for nest_pred_scale_i in nest_pred_scale:
-                                                    sim_counter += 1
-                                                    output_file = base_name + "_" + str(sim_counter)
+        mu_b_resource_i = mu_a_resource_i
 
-                                                    print(f"{exe} " +
-                                                          f"{output_file} " +
-                                                          f"{mu_a_resource_i} " + 
-                                                          f"{mu_b_resource_i} " + 
-                                                          f"{mu_a_t_i} " + 
-                                                          f"{mu_b_t_i} " + 
-                                                          f"{mu_a_qown_i} " + 
-                                                          f"{mu_b_qown_i} " + 
-                                                          f"{mu_a_qother_i} " + 
-                                                          f"{mu_b_qother_i} " + 
-                                                          f"{mu_a_action_other_i} " + 
-                                                          f"{mu_b_action_other_i} " + 
-                                                          f"{p_high_quality_i} " + 
-                                                          f"{init_resources} " + 
-                                                          f"{nest_pred_baseline} " + 
-                                                          f"{nest_pred_scale_i} " + 
-                                                          f"{max_gen} ") 
+        for mu_a_t_i in mu_a_t:
+            mu_b_t_i = mu_a_t_i
+
+            for mu_a_qown_i in mu_a_qown:
+                mu_b_qown_i = mu_a_qown_i
+
+                for mu_a_qother_i in mu_a_qother:
+                    mu_b_qother_i = mu_a_qother_i
+
+                    for mu_a_action_other_i in mu_a_action_other:
+                        mu_b_action_other_i = mu_a_action_other_i
+
+                        for p_high_quality_i in p_high_quality:
+                            for nest_pred_scale_i in nest_pred_scale:
+                                sim_counter += 1
+                                output_file = base_name + "_" + str(sim_counter)
+
+                                print(f"{exe} " +
+                                      f"{output_file} " +
+                                      f"{mu_a_resource_i} " + 
+                                      f"{mu_b_resource_i} " + 
+                                      f"{mu_a_t_i} " + 
+                                      f"{mu_b_t_i} " + 
+                                      f"{mu_a_qown_i} " + 
+                                      f"{mu_b_qown_i} " + 
+                                      f"{mu_a_qother_i} " + 
+                                      f"{mu_b_qother_i} " + 
+                                      f"{mu_a_action_other_i} " + 
+                                      f"{mu_b_action_other_i} " + 
+                                      f"{p_high_quality_i} " + 
+                                      f"{init_resources} " + 
+                                      f"{nest_pred_baseline} " + 
+                                      f"{nest_pred_scale_i} " + 
+                                      f"{max_gen} ") 
 
 
-                
+
